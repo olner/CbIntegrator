24956aa4a6d42bd7ebc1ad5606b350c1ef72ba03
olner <izmailov2003@yandex.ru>
2023-01-21 14:19:21 +0300

    Добавил свой exception

---
+++ CbIntegrator/CbIntegrator.Bussynes/Exceptions/CbServiceExceptions/NoConnectionException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CbIntegrator.Bussynes.Exceptions.CbServiceExceptions
+{
+    /// <summary>
+	/// Отсутвует подключение к интернету
+	/// </summary>
+    public class NoConnectionException : ServiceException
+    {
+        public NoConnectionException() : base("No Internet Connections")
+        {
+        }
+    }
+}
